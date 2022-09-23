5048c3500edb2b01b2ecdfbe6476368d0da3825d
Yegor Pelykh <yegor.dev@gmail.com>
2022-09-23 18:30:44 +0300

    feat: New action / pageAction functionality, reworked service worker script

--- web_ts/service_worker.ts
+++ web_ts/service_worker.ts
@@ -10,33 +10,31 @@ type MessageConfirmation = {
 }
 type MessageHandler = (port: chrome.runtime.Port, data: any, error?: string) => Promise<any>;
 
-const connectedPorts = new Set<chrome.runtime.Port>();
-const awaitingMessages: Map<string, MessageConfirmation> = new Map<string, MessageConfirmation>();
-const messageHandlers = new Map<string, MessageHandler>([]);
-
-function genUUID(): string {
-    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+abstract class Helpers {
+    static genUUID(): string {
+        const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+    }
 }
 
-chrome.runtime.onConnect.addListener((port) => {
-    connectedPorts.add(port);
-    port.onDisconnect.addListener((port) => {
-        connectedPorts.delete(port);
-    });
-    port.onMessage.addListener((message: Message, port) => {
+abstract class ConnectionManager {
+    private static connectedPorts: Set<chrome.runtime.Port> = new Set<chrome.runtime.Port>();
+    private static awaitingMessages: Map<string, MessageConfirmation> = new Map<string, MessageConfirmation>();
+    private static messageHandlers: Map<string, MessageHandler> = new Map<string, MessageHandler>([]);
+
+    private static messageListener(message: Message, port: chrome.runtime.Port): void {
         if ('uuid' in message && 'type' in message && 'data' in message) {
             const { uuid, type, data, error } = message;
-            const confirmation = awaitingMessages.get(uuid);
+            const confirmation = this.awaitingMessages.get(uuid);
             if (confirmation != null) {
                 if (error != null) {
                     confirmation.reject(error);
                 } else {
                     confirmation.resolve(data);
                 }
-                awaitingMessages.delete(uuid);
+                this.awaitingMessages.delete(uuid);
             } else {
-                const handler = messageHandlers.get(type);
+                const handler = this.messageHandlers.get(type);
                 if (handler != null) {
                     handler(port, data, error).then((responseData) => {
                         port.postMessage(<Message>{
@@ -61,27 +59,115 @@ chrome.runtime.onConnect.addListener((port) => {
                 }
             }
         }
-    });
-});
+    }
 
-async function sendMessage(port: chrome.runtime.Port, type: string, data: any, error?: string): Promise<any> {
-    const uuid = genUUID();
-    const promise = new Promise<any>((resolve, reject) => {
-        awaitingMessages.set(uuid, <MessageConfirmation>{
-            resolve: resolve,
-            reject: reject,
+    private static handlePortDisconnection(port: chrome.runtime.Port): void {
+        this.connectedPorts.delete(port);
+    }
+
+    static handlePortConnection(port: chrome.runtime.Port): void {
+        this.connectedPorts.add(port);
+        port.onDisconnect.addListener(this.handlePortDisconnection);
+        port.onMessage.addListener(this.messageListener);
+    }
+
+    static async sendMessage(port: chrome.runtime.Port, type: string, data: any, error?: string): Promise<any> {
+        const uuid = Helpers.genUUID();
+        const promise = new Promise<any>((resolve, reject) => {
+            this.awaitingMessages.set(uuid, <MessageConfirmation>{
+                resolve: resolve,
+                reject: reject,
+            });
+            port.postMessage(<Message>{
+                uuid: uuid,
+                type: type,
+                data: data,
+                error: error,
+            });
         });
-        port.postMessage(<Message>{
-            uuid: uuid,
-            type: type,
-            data: data,
-            error: error,
-        });
-    });
-    return promise;
+        return promise;
+    }
+
+    static async broadcastMessage(type: string, data: any, error?: string): Promise<PromiseSettledResult<Object>[]> {
+        const promises = Array.from(this.connectedPorts.values()).map(p => this.sendMessage(p, type, data, error));
+        return await Promise.allSettled(promises);
+    }
+
 }
 
-async function broadcastMessage(type: string, data: any, error?: string): Promise<PromiseSettledResult<Object>[]> {
-    const promises = Array.from(connectedPorts.values()).map(p => sendMessage(p, type, data, error));
-    return await Promise.allSettled(promises);
+abstract class ActionHandler {
+    private static allowedSchemes: string[] = [
+        'https',
+        'http',
+    ];
+
+    static setV2ActionRules() {
+        if (chrome.pageAction == null) {
+            return;
+        }
+        const applyPageActionVisibility = (tab: chrome.tabs.Tab) => {
+            const isUrlAllowed = (url: string) => {
+                return this.allowedSchemes.some((scheme: string) => {
+                    return url.toLowerCase().startsWith(`${scheme.toLowerCase()}:`);
+                });
+            };
+            if (tab.id != null && tab.url != null) {
+                if (isUrlAllowed(tab.url)) {
+                    chrome.pageAction.show(tab.id);
+                } else {
+                    chrome.pageAction.hide(tab.id);
+                }
+            }
+        }
+        chrome.tabs.query({}, (tabs) => {
+            for (const tab of tabs) {
+                applyPageActionVisibility(tab);
+            }
+        });
+        chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) => {
+            applyPageActionVisibility(tab);
+        });
+    }
+
+    static setV3ActionRules() {
+        if (chrome.action == null || chrome.declarativeContent == null) {
+            return;
+        }
+        chrome.action.disable();
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+            const rules = [
+                {
+                    conditions: [
+                        new chrome.declarativeContent.PageStateMatcher({
+                            pageUrl: {
+                                schemes: this.allowedSchemes
+                            },
+                        })
+                    ],
+                    actions: [
+                        new chrome.declarativeContent.ShowAction(),
+                    ],
+                },
+            ];
+            chrome.declarativeContent.onPageChanged.addRules(rules);
+        });
+    }
 }
+
+abstract class LifecycleHandler {
+    private static onInstalledListener() {
+        ActionHandler.setV2ActionRules();
+        ActionHandler.setV3ActionRules();
+    }
+
+    private static onPortConnectedListener(port: chrome.runtime.Port) {
+        ConnectionManager.handlePortConnection(port);
+    }
+
+    static init(): void {
+        chrome.runtime.onInstalled.addListener(this.onInstalledListener);
+        chrome.runtime.onConnect.addListener(this.onPortConnectedListener);
+    }
+}
+
+LifecycleHandler.init();
