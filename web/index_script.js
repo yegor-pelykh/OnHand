74d7be96a7046ee05ff510739ef4168ddd7b7b8c
Yegor Pelykh <yegor.dev@gmail.com>
2022-09-24 14:19:56 +0300

    perf: Removed dependence on the file flutter.js, removed background flicker when loading the page

--- web/index_script.js
+++ web/index_script.js
@@ -1,15 +1,19 @@
 const bgColorLight = '#fcfcfd';
 const bgColorDark = '#1f1b17';
+const appScriptName = 'main.dart.js';
 
 function setBgColor() {
     window.document.body.style.backgroundColor = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? bgColorDark : bgColorLight;
 }
 
-window.addEventListener('load', function (ev) {
+function loadApp() {
+    let scriptTag = document.createElement("script");
+    scriptTag.src = appScriptName;
+    scriptTag.type = "application/javascript";
+    document.body.append(scriptTag);
+}
+
+window.addEventListener('load', function (_) {
     setBgColor();
-    _flutter.loader.loadEntrypoint({}).then(function (engineInitializer) {
-        return engineInitializer.initializeEngine();
-    }).then(function (appRunner) {
-        return appRunner.runApp();
-    });
+    loadApp();
 });
